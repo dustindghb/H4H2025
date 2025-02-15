7efae6a84e2a6b2f04ea4971ee94acee68552b5b
Keaan Mirchandani <keaanm@outlook.com>
2025-02-15 12:12:16 -0800

    second commit

---
+++ src/app/api/[[...route]]/route.ts
@@ -0,0 +1,16 @@
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import { z } from "zod";
+
+export const runtime = "edge";
+
+const app = new Hono().basePath("/api");
+
+app.get("/hello", (c) => {
+  return c.json({
+    message: "Hello Next.js!",
+  });
+});
+
+export const GET = handle(app);
+export const POST = handle(app);
